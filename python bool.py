4e83b5eb2d2a294b7fb0391e22cc317a03eeb737
k-dong2307 <piopioccchris6160@gmail.com>
2026-03-28 11:11:05 +0900

    Add files via upload

---
+++ python bool.py
@@ -0,0 +1,9 @@
+a = True
+b = False
+
+c = bool(0)
+d = bool(1)
+e = bool(-1)
+f = bool(10)
+
+print(a, b, c, d, e, f)
---
+++ python dictionary.py
@@ -0,0 +1,10 @@
+my_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "is_student": False
+}
+
+print(my_dict)
+print("Name:", my_dict["name"])
+print("Age:", my_dict["age"])
