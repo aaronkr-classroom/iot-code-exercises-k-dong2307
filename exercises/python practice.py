dcb70baaf632454ed8ca16c8ebfda426331cd4de
k-dong2307 <piopioccchris6160@gmail.com>
2026-04-06 20:15:05 +0900

    chap3

---
+++ exercises/python practice.py
@@ -0,0 +1,11 @@
+sensors = {
+    "dht11" : {
+        "temperature" : 23,
+        "humidity" : 47,
+        "unit" : "celsius"
+    },
+    "bh1750" : {
+        "lux" : 450,
+        "unit" : "lux"
+    }
+}
---
+++ exercises/python tuple.py
@@ -0,0 +1,11 @@
+red = (255, 0 ,0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+print("Red:", red)
+print("Green:", green)
+print("blue:", blue)
+print("white:", white)
+print("Black:", black)
