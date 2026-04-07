7856d4a47fc7f629d9bad7cd244732d76cb961dc
k-dong2307 <piopioccchris6160@gmail.com>
2026-04-07 22:05:23 +0900

    Add files via upload

---
+++ code/python def.py
@@ -0,0 +1,16 @@
+def  print_info(name, phone, address, email):
+    contact_info = f"연락처: {phone}\n이메일: {email}
+    return f"이름:\t{name}\n{contact}
+
+"""
+안녕하세요 교수님. 학번 2544003 김동현 입니다.
+다름이아니라 저는 노트북도 없고, 학교 컴퓨터도 작동하는게 많지않아서
+대부분 손으로 실습을 작성하고 집에가서 코딩하여 실습과제를 올립니다.
+그런데 가끔씩 속도를 놓쳐서 끝까지 작성하지 못한 실습들이 있습니다.
+미완성으로 실습과제를 올리는점 양해 부탁드립니다. 죄송합니다.
+
+I don’t have a laptop, and even the computers at school are not always working.
+Even if I change seats, there are only a few computers that function properly.
+Because of this, I write code by hand and complete it at home before uploading my assignments.
+However, I wasn’t able to finish all of the exercises, so I kindly ask for your understanding regarding the incomplete submissions.
+"""
